bed6208d42e0c817323244c311c06f4ca25febf7
sooooz <soojinpar1026@gmail.com>
2020-06-11 19:32:42 +0900

    Django_Faker

--- Django_Blog(2)/blogproject_SJ/portfolioapp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Portfolio
-
-def portfolio(request):
-    portfolios = Portfolio.objects
-    return render(request, 'portfolio.html', {'portfolios':portfolios})
-
-    
